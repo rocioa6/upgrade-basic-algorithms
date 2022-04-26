54d4da06690ca89cd992722ec62f0c3d0687113d
rocioa6 <rocioa6@gmail.com>
2022-04-26 23:53:44 +0200

    Iteration#5 fix

--- index.js
+++ index.js
@@ -87,11 +87,11 @@ if (number2 / number1 == 2) {
   console.log("number2 dividido entre number1 es igual a 2");
 }
 
-if (number1 === !number2) {
+if (number1 !== number2) {
   console.log("number1 es estrictamente distinto a number2");
 }
 
-if (number3 == !number1) {
+if (number3 != number1) {
   console.log("number3 es distinto number1");
 }
 
